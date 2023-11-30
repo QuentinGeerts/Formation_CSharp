6e56e29d23036245c7ebba8d62a69130131da1e1
QuentinGeerts <quentin.geerts@bstorm.be>
2023-11-30 02:02:38 +0100

    .

---
+++ DemoDelegues/NotificationService.cs
@@ -0,0 +1,32 @@
+﻿namespace DemoDelegues;
+
+// Définition du délégué pour les méthodes de notification
+public delegate void NotificationDelegate(string message);
+
+public class NotificationService
+{
+    // Délégué pour la méthode de notification
+    private NotificationDelegate _notificationDelegate;
+    
+    // Méthode pour enregistrer la méthode de notification
+    public void RegisterNotification(NotificationDelegate method)
+    {
+        _notificationDelegate += method;
+    }
+
+    // Méthode pour désenregistrer la méthode de notification
+    public void UnregisterNotification(NotificationDelegate method)
+    {
+        _notificationDelegate -= method;
+    }
+    
+    // Méthode pour exécuter le système de notification
+    public void SendNotification(string message)
+    {
+        if (_notificationDelegate != null)
+            // _notificationDelegate(message);
+            _notificationDelegate.Invoke(message);
+        
+        // _notificationDelegate?.Invoke(message);
+    }
+}
--- DemoDelegues/Program.cs
+++ DemoDelegues/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/* ----------------------------------------------------------------
+ * › Chapitre 14 - Les délégués
+ */
 
-Console.WriteLine("Hello, World!");
+using DemoDelegues;
+
+var notificationService = new NotificationService();
+
+// Enregistrer les méthodes de notification personnalisées
+notificationService.RegisterNotification(SendEmail);
+notificationService.RegisterNotification(SendSMS);
+
+// Envoi des notifications
+notificationService.SendNotification("Notification envoyée : 'Hello !'"); // Email + SMS
+
+Console.WriteLine("---");
+
+notificationService.UnregisterNotification(SendEmail);
+notificationService.SendNotification("Notification envoyée : 'Seulement par SMS !'"); // SMS
+
+
+
+static void SendEmail(string message)
+{
+ Console.WriteLine("Envoi d'email : " + message);
+}
+
+static void SendSMS(string message)
+{
+ Console.WriteLine("Envoi de SMS : " + message);
+}
